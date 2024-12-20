77c3ba6148f617141c11c1297fa83f94140f745c
zynxoso <107228613+zynxoso@users.noreply.github.com>
2024-12-20 18:23:17 +0800

    Add files via upload

---
+++ includes/scripts.php
@@ -0,0 +1,27 @@
+        <!-- Camera -->
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.24/webcam.js"></script>
+        <!-- Alternative CSS -->
+        <link href="../../includes/assets/css/style2.css" rel="stylesheet" />
+        <!-- Include Flatpickr CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+        <!-- Bootstrap 5.1.3 bundle -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- jquery CDN link -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" ></script>
+        <!-- Header and sidenav CSS -->
+        <link rel="stylesheet" href="../../includes/assets/css/styles.css">
+        <!-- FontAwesome -->
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Script for toggle button -->
+        <script src="../../includes/js/scripts.js"></script>
+        <!-- Script for search  bar -->
+        <script src="../../includes/js/search.js"></script>
+        <!-- Bootstrap icons 5.1.3 -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
+        <!-- FontAwesome -->
+        <script src="https://kit.fontawesome.com/a26cd4790f.js" crossorigin="anonymous"></script>
+        <!-- Ajax -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js"></script>
+        
+    </body>
+</html>
